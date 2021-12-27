11e655bf5cfff883e8f65c9e4692d3215b301803
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2021-12-26 23:15:46 -0300

    ok

---
+++ js/script.js
@@ -0,0 +1,31 @@
+one.style.color = 'white'
+two.style.color = 'white'
+three.style.color = 'white'
+four.style.color = 'white'
+
+function entrar1() {
+    one.innerHTML = `Você está no primeiro andar`
+}
+
+function entrar2() {
+    two.innerHTML = `Você está no segundo andar`
+}
+
+function entrar3() {
+    three.innerHTML = `Você está no terceiro andar`
+}
+
+function entrar4() {
+    four.innerHTML = `Você está no quarto andar`
+}
+
+function sair() {
+    one.innerHTML = `1°`
+    two.innerHTML = `2°`
+    three.innerHTML = `3°`
+    four.innerHTML = `4°`
+}
+
+
+
+
